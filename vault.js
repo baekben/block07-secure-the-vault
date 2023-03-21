e2434b46070b1100c3d3ee1081bbc7a707f60e6a
Ben Baek <benbaeksea@gmail.com>
2023-03-21 16:44:28 -0700

    Uploading block07 workshop

---
+++ vault.js
@@ -0,0 +1,18 @@
+// Have a variable equal 10 using arithmetic operators and integers
+let c1 = 5 * 2;
+
+// Have a variable equal 40 using arithmetic operators and integers
+let c2 = 30 + c1;
+
+// Have a variable equal 39 using arithmetic operators and integers
+let c3 = c2 - 1;
+
+// Create a string that contains the message to the user with the code
+let secretMsg = `"You have received this message becasue you have been chosen to open an important vault. Here is the secret combination: ${c1}-${c2}-${c3}"`;
+
+// Display the message on the html page
+alert(secretMsg);
+
+console.log("== If you see this message... ==");
+console.log("== Be careful. Security is tight ==");
+console.log("== Grab what you need and leave ==");
